a9306b757fea1d84e8995d5440c923c3fb4f0c94
Felix Kreuk <felixkreuk@fb.com>
2022-03-30 19:09:23 +0300

    added same sentence samples

---
+++ emotion/folders_to_rows.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import uuid
+
+
+template = """
+<td>
+    <div id="{id}"></div>
+    <button id="{button_id}" class="play-button-demo btn btn-primary" onclick="{var}.playPause()"> <i class="fa fa-play"></i> Play / <i class="fa fa-pause"></i> Pause </button>
+    <script> var {var} = WaveSurfer.create({{ container: '#{id}', waveColor: 'violet', progressColor: 'purple' }});
+        {var}.load('{wav_path}'); </script>
+</td>
+"""
+root = Path("./audio/same_sentence/female")
+
+wavs = root.glob("*.wav")
+out = ""
+
+for wav in wavs:
+    uid = uuid.uuid4().hex.upper()[0:6]
+    button_uid = uuid.uuid4().hex.upper()[0:6]
+    var_uid = uuid.uuid4().hex.upper()[0:6]
+    out += template.format(id=uid, var=var_uid, button_id=button_uid, wav_path=str(wav)) + "\n"
+print(out)
